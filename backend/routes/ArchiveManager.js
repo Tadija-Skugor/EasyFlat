4333589a7615b8836f655dd41c7455f272182aab
MirtaKrajacic <mirta.krajacic@fer.hr>
2025-01-03 21:36:12 +0100

    Implementacija funkcija za arhivu
    Implementacija funkcija getAllArchived i getArchived te postavljanje njihovih ruta u ArchiveManager datoteci.

--- backend/routes/ArchiveManager.js
+++ backend/routes/ArchiveManager.js
@@ -7,7 +7,7 @@ class ArchiveManager {
     this.initializeRoutes();
   }
 
-  async checkforArchiving() {         //funkcija koja se izvrsava jednom na dan i prebacuje stare diskusije u arhivu
+  async checkforArchiving() {         // funkcija koja se izvrsava jednom na dan i prebacuje stare diskusije u arhivu
     try {
       const result = await pool.query(`DELETE FROM diskusija WHERE zadnji_pristup <= CURRENT_DATE - INTERVAL '30 days' RETURNING *`);
 
@@ -38,17 +38,73 @@ class ArchiveManager {
     
   }
 
-  async getAllArchived(req, res) {
-    console.log("iz get /archive")      //funkcija za dohvat svih (ili prvih 10 kako vec) diskusija iz arhive
+  async getAllArchived(req, res) { // funkcija za dohvat diskusija iz arhive
+    console.log("iz get /archive/allArchived")    
+
+    try {
+      // broj diskusija za dohvatiti iz arhive ili default 10
+      const brojZatrazenihDiskusija = req.query.brojZatrazenihDiskusija || 10;
+
+      const result = await pool.query(
+        'SELECT id, naslov, opis, kreator, datum_stvorena, naslov_glasanja, glasovi_da, glasovi_ne FROM arhiva ORDER BY datum_stvorena DESC LIMIT $1;',
+        [brojZatrazenihDiskusija]
+      );
+
+      // lista svih diskusija iz arhive
+      const discussionList = await Promise.all(result.rows.map(async (row) => {
+        const discussion = {
+          id: row.id,
+          naslov: row.naslov,
+          opis: row.opis,
+          kreator: row.kreator,
+          datum_stvorena: row.datum_stvorena,
+        };
+
+        // samo da diskusije koje imaju povezano glasanje se salju ovi atributi 
+        if (row.naslov_glasanja !== null) {
+          discussion.naslov_glasanja = row.naslov_glasanja;
+          discussion.glasovi_da = row.glasovi_da;
+          discussion.glasovi_ne = row.glasovi_ne;
+        }
+
+        return discussion;
+      }));
+
+      // slanje liste diskusija 
+      res.json(discussionList);
+    } catch (error) {
+      console.error("Greška u /archive/allArchived:", error.message);
+      res.status(500).send('Greška na serveru');
+    }
   }
 
-  async getArchived(req, res){        //funkcija za dohvat svih podataka specificne diskusije iz arhive
-    console.log("iz post /archive")
+  async getArchived(req, res){        // funkcija za dohvat odgovora diskusije zadanog id-a iz arhive
+    console.log("iz get /archive/archivedResponses")
+
+    try {
+      const id_diskusije = req.query.id_diskusije;
+      
+      const result = await pool.query(
+        'SELECT odgovori FROM arhiva WHERE id=$1;', [id_diskusije]
+      );
+
+      if (result.rows[0]) {
+        const odgovori = result.rows[0].odgovori;
+        res.json(odgovori);
+      } else {
+        res.json('Polje odgovora je prazno');
+      }
+      
+    } catch (error) {
+      console.error("Greška u /archive/archivedResponses", error.message);
+      res.status(500).send('Greška na serveru');
+    }
+  
   }
 
   initializeRoutes() {
-    this.router.get('/', this.getAllArchived.bind(this));
-    this.router.post('/', this.getArchived.bind(this));
+    this.router.get('/allArchived', this.getAllArchived.bind(this));
+    this.router.get('/archivedResponses', this.getArchived.bind(this));
   }
 }
 
